7d26728a33d9c40737af2485984df8b7233998ce
dstmp <dsucher+dstmp@gmail.com>
2014-03-19 02:38:05 -0400

    Clarify comment for profile output format string

--- profile.sh
+++ profile.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# user time, system time, realtime, maximum resident set
+# user time, system time, realtime, % CPU usage, maximum resident set, command being timed
 FORMAT='%Uu %Ss %er %MkB %Pcpu %C'
 
 /usr/bin/time -f "$FORMAT" python ./python/psort.py --algorithm heapsort < $@ > /dev/null
